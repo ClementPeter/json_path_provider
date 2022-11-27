0b7e1c7110063a8bb73ec64eb8c8639561c314d9
ClementPeter <61213263+ClementPeter@users.noreply.github.com>
2022-11-27 21:54:31 +0100

    Remote Services, Caching and Depedency

--- lib/main.dart
+++ lib/main.dart
@@ -1,73 +1,73 @@
-// import 'package:flutter/material.dart';
-// import 'package:json_path_provider/details_page.dart';
-// import 'package:json_path_provider/networking.dart';
+import 'package:flutter/material.dart';
+import 'package:json_path_provider/details_page.dart';
+import 'package:json_path_provider/networking.dart';
 
-// void main() {
-//   runApp(const MyApp());
-// }
+void main() {
+  runApp(const MyApp());
+}
 
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Json Cache_Path Provider',
-//       theme: ThemeData(
-//         primarySwatch: Colors.blue,
-//       ),
-//       home: const MyHomePage(title: 'Flutter Demo Home Page'),
-//     );
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Json Cache_Path Provider',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
 
-// //Building UI using Future
-// class MyHomePage extends StatefulWidget {
-//   const MyHomePage({super.key, required this.title});
+//Building UI using Future
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
 
-//   final String title;
+  final String title;
 
-//   @override
-//   State<MyHomePage> createState() => _MyHomePageState();
-// }
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
 
-// class _MyHomePageState extends State<MyHomePage> {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text('Json Cache_Path Provider'),
-//       ),
-//       body: FutureBuilder(
-//         future: ApiProvider().getData(),
-//         builder: ((context, snapshot) {
-//           if (!snapshot.hasData) {
-//             return Center(
-//               child: CircularProgressIndicator(),
-//             );
-//           } else {
-//             return ListView.builder(
-//               itemCount: 10,
-//               itemBuilder: ((context, index) {
-//                 return ListTile(
-//                   title: Text("iowf"),
-//                   subtitle: Text(""),
-//                   onTap: () {
-//                     Navigator.push(
-//                       context,
-//                       MaterialPageRoute(
-//                         builder: (_) {
-//                           return DetailsPage();
-//                         },
-//                       ),
-//                     );
-//                   },
-//                 );
-//               }),
-//             );
-//           }
-//         }),
-//       ),
-//     );
-//   }
-// }
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Json Cache_Path Provider'),
+      ),
+      body: FutureBuilder(
+        future: ApiProvider().getData(),
+        builder: ((context, snapshot) {
+          if (!snapshot.hasData) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          } else {
+            return ListView.builder(
+              itemCount: 10,
+              itemBuilder: ((context, index) {
+                return ListTile(
+                  title: Text("iowf"),
+                  subtitle: Text(""),
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (_) {
+                          return DetailsPage();
+                        },
+                      ),
+                    );
+                  },
+                );
+              }),
+            );
+          }
+        }),
+      ),
+    );
+  }
+}
